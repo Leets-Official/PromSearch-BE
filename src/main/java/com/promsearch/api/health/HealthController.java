7ecb8c45993d31fb946ca59ec8fe8d10d4b6d847
Hanharam <hanharamm@naver.com>
2026-07-05 22:05:28 +0900

    [Docs] 템플릿 및 초기세팅

--- src/main/java/com/promsearch/api/health/HealthController.java
+++ src/main/java/com/promsearch/api/health/HealthController.java
@@ -1,4 +1,4 @@
-package com.promsearch.backend.api.health;
+package com.promsearch.api.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
