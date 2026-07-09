6093d98e41762b1b32e5e1b87e735efd5a7da1e5
ruchan04 <ruchan0@gachon.ac.kr>
2026-07-09 09:48:57 +0900

    feat: implement user profile update and deletion APIs

--- src/main/java/com/promsearch/user/application/port/out/UserRepository.java
+++ src/main/java/com/promsearch/user/application/port/out/UserRepository.java
@@ -6,7 +6,20 @@ public interface UserRepository {
 
     User create(User user);
 
+    User getById(Long userId);
+
     boolean existsByNickname(String nickname);
 
     boolean existsByEmail(String email);
+
+    User updateProfile(
+            Long userId,
+            String email,
+            String password,
+            String nickname,
+            String name,
+            String profileImageUrl
+    );
+
+    void deleteById(Long userId);
 }
